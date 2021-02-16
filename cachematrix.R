188d2c5740e59afcd07a0ca419542c690d70f07b
iskakanartur <artdar14@gmail.com>
2021-02-16 11:42:47 +0300

    First Commit R assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Makes a matrix, with properties (methods as in Python) to get the matrix itself as well as the reverse of it
 
 makeCacheMatrix <- function(x = matrix()) {
+  reversed <- NULL
+  set <- function(y) {
+    x <<- y
+    reversed <<- NULL
+  }
+  get <- function() x
+  setrev <- function(inverse) reversed <<- inverse
+  getrev <- function() reversed
+  list(set = set, get = get, setrev= setrev, getrev= getrev)
 
 }
 
 
-## Write a short comment describing this function
+## If the reversed matrix is present the function just returns it without computing it again, Otherwise computes the reverse 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  reversed<- x$getrev ()
+  if (!is.null(reversed)) {
+    message("Getting your reversed matrix from cache")
+    return(reversed)
+  }
+  mt <- x$get ()
+  reversed <- solve(mt, ...)
+  x$setrev(reversed)
+  reversed
 }
